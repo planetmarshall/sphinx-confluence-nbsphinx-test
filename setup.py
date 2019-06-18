30b582cd3d2ef0842d494bc03369bf86b5894a67
James Knight <james.d.knight@live.com>
2019-06-17 23:50:46 -0400

    sphinx-confluence-nbsphinx-test
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# This is free and unencumbered software released into the public domain.
+
+from setuptools import find_packages
+from setuptools import setup
+
+requires = [
+    'ipypublish',
+    'nbsphinx',
+    'sphinx',
+    'sphinxcontrib-confluencebuilder',
+    ]
+
+setup(
+    name='sphinx-confluence-nbsphinx-test',
+    version='0.0.0-dev0',
+    #author='???',
+    #author_email='???',
+    description="""Sphinx extension to support nbsphinx with the Sphinx """
+                """Confluence extension.""",
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
---
+++ testdoc/conf.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# This is free and unencumbered software released into the public domain.
+
+extensions = [
+    'nbsphinx',
+    'sphinxcontrib.confluencebuilder',
+    'sphinxcontrib.confluencebuilder_nbsphinx',
+]
+
+master_doc = 'index'
